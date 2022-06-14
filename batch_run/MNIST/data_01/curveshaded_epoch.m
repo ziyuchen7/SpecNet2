445fd493da2c3f00b3bb4c8dfc4a7af7d215b9bd
Ziyu Chen <46736533+ziyuchen7@users.noreply.github.com>
2022-06-14 14:23:03 -0400

    Create curveshaded_epoch.m

---
+++ batch_run/MNIST/data_01/curveshaded_epoch.m
@@ -0,0 +1,83 @@
+% clear; close all;
+c = zeros(6,1);
+d = zeros(6,1);
+epoch = zeros(6,1);
+batch = zeros(6,1);
+c(1) = 3; c(2) = 2.6; c(3) = 2.1; c(4) = 31; c(5) = 15.7; c(6) = 9.9;
+epoch(1) = 800; epoch(2) = 923; epoch(3) = 1142; epoch(4) = 150; epoch(5) = 152; epoch(6) = 242;
+batch(1) = 45; batch(2) = 90; batch(3) = 180; batch(4) = 2; batch(5) = 4; batch(6) = 8;
+
+color = [ 0 0.4470 0.7410;
+          0.8500 0.3250 0.0980;
+          0.9290 0.6940 0.1250;
+          0.4940 0.1840 0.5560;
+          0.4660 0.6740 0.1880;
+          0.3010 0.7450 0.9330];
+
+for k = 1:6
+    acc_mean = zeros(2,epoch(k));
+    acc_std  = zeros(2,epoch(k));
+    acc_assort1 = zeros(10,epoch(k));
+    acc_assort2 = zeros(10,epoch(k));
+    
+    if k <= 3
+        grad_type = 'specnet1_localu';
+    else
+        grad_type = 'specnet2_neighbor';
+    end
+
+    for i = 1:10
+        if k == 4
+            netout = load([grad_type '_unit_256_depth_3_lr_0.0001' '_batch_' num2str(batch(k),'%d')...
+            '-' num2str(i+9,'%d') '.mat']);
+        else
+            netout = load([grad_type '_unit_256_depth_3_lr_0.0001' '_batch_' num2str(batch(k),'%d')...
+            '-' num2str(i-1,'%d') '.mat']);
+        end
+        acc_assort1(i,:) = netout.grad_loss(1,:);
+        acc_assort2(i,:) = netout.grad_loss(2,:);
+    end
+    
+    acc_mean(1,:) = mean(acc_assort1,1);
+    acc_mean(2,:) = mean(acc_assort2,1);
+    acc_std(1,:)  = std(acc_assort1);
+    acc_std(2,:)  = std(acc_assort2);
+    
+    x = 1:epoch(k);
+    x2 = [x, fliplr(x)];
+    inBetween1 = [log10(acc_mean(1,:) + acc_std(1,:)), log10(fliplr(acc_mean(1,:) - acc_std(1,:)))];
+    inBetween2 = [log10(acc_mean(2,:) + acc_std(2,:)), log10(fliplr(acc_mean(2,:) - acc_std(2,:)))];
+%     inBetween1 = [acc_mean(1,:) + acc_std(1,:), fliplr(acc_mean(1,:) - acc_std(1,:))];
+%     inBetween2 = [acc_mean(2,:) + acc_std(2,:), fliplr(acc_mean(2,:) - acc_std(2,:))];
+    figure(1)
+    fill(x2, inBetween1, color(k,:),'FaceAlpha', 0.3,'linestyle','none');
+    hold on
+    plot(x, log10(acc_mean(1,:)), 'color', color(k,:), 'LineWidth', 2);
+%     plot(x, acc_mean(1,:), 'color', color(k,:), 'LineWidth', 2);
+
+    figure(2)
+    fill(x2, inBetween2, color(k,:),'FaceAlpha', 0.3,'linestyle','none');
+    hold on
+    plot(x, log10(acc_mean(2,:)), 'color', color(k,:), 'LineWidth', 2);
+%     plot(x, acc_mean(2,:), 'color', color(k,:), 'LineWidth', 2);
+
+end
+figure(1)
+legend('','SpecNet1-local bs=45','','SpecNet1-local bs=90','','SpecNet1-local bs=180',...
+    '','SpecNet2-neighbor bs=2','','SpecNet2-neighbor bs=4','','SpecNet2-neighbor bs=8')
+set(gca,'fontsize',24)
+xlabel('Epoch')
+ylabel('$\log_{10}$','Interpreter','latex')
+xlim([0 150])
+ylim([-4 1])
+title('Loss $f_1$','Interpreter','latex')
+
+figure(2)
+legend('','SpecNet1-local bs=45','','SpecNet1-local bs=90','','SpecNet1-local bs=180',...
+    '','SpecNet2-neighbor bs=2','','SpecNet2-neighbor bs=4','','SpecNet2-neighbor bs=8')
+set(gca,'fontsize',24)
+xlabel('Epoch')
+ylabel('$\log_{10}$','Interpreter','latex')
+xlim([0 150])
+ylim([-4 1])
+title('Loss $f_2$','Interpreter','latex')
